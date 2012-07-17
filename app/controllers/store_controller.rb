207accfc3eec206cb72f8e33563f4c4870b03218
John McCormack <jmccormack321@gmail.com>
2012-07-17 23:57:37 +0100

    fixed sessions & search issues

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,7 @@ class StoreController < ApplicationController
   def index   #gets products & displays them
     @products = Product.search(params[:search_query])
 
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @products }
