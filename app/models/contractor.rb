9c83e73d081ffba08278f68402689938618e939f
Enda McDonagh <oisin123@eircom.net>
2012-07-20 12:38:39 +0100

    select location updated

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,4 +1,27 @@
 class Contractor < ActiveRecord::Base
+  #@author Enda McDonagh X12100871
+  #@Ref Ruby on Rails Tutorial by Michael Hartl
   attr_accessible :email, :jobtype, :location, :name
   has_many :quotations
+  validate :checked_location
+  validate :checked_skill
+  validates :email, presence: true, length: { minimum: 1 }
+  validates :jobtype, presence: true, length: { minimum: 1 }
+  validates :location, presence: true, length:{minimum: 1}
+  validates :name, presence: true, length: { minimum: 1 }
+
+  def checked_location
+   if location == "Select One"
+      errors.add(:location, "You must select a County")
+   end
+  def checked_skill
+    if jobtype == "Select One"
+      errors.add(:jobtype, "You must select a Skill")
+    end
+
+  end
+    end
+
+
+
 end
